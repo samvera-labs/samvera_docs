0f44b5cf7b910f315b07bde74430aa5b7569e37f
Andrew Myers <andrew_myers@wgbh.org>
2019-07-16 14:39:56 -0400

    Refactor Publisher#publish
    Also removes debug msg.

--- lib/publisher.rb
+++ lib/publisher.rb
@@ -1,14 +1,18 @@
 require 'tmpdir'
 
 class Publisher
+  attr_reader :tmp_dir
 
   def publish
-    # create_tmp_dir
-    # git_clone
-    # copy_site
-    # git_commit
-    # git_push
-    # delete_tmp_dir
+    @tmp_dir = Dir.mktmpdir
+    `git clone #{gh_pages_repo} #{@tmp_dir}`
+    `bundle exec jekyll build -d #{@tmp_dir}`
+    Dir.chdir @tmp_dir do
+      `git add .`
+      `git commit -m "Publishing with Publisher tool."`
+      `git push`
+    end
+    # `rm -rf #{@tmp_dir}`
   end
 
   def self.publish
@@ -16,4 +20,8 @@ class Publisher
   end
 
   private
+
+    def gh_pages_repo
+      'git@github.com:afred/afred.github.io.git'
+    end
 end
--- tasks/publish.rake
+++ tasks/publish.rake
@@ -1,5 +1,3 @@
-puts "LOAD_PATH = #{$LOAD_PATH}"
-
 require 'publisher'
 
 desc 'publishes static site to Github'
